c066156f6066bed35b5ca26e0626815866eb8e0d
Amaredeus <patinobrian@gmail.com>
2016-09-29 21:20:35 -0400

    add first multi-threaded program

--- Assignment_2/helloMultiThreaded.c
+++ Assignment_2/helloMultiThreaded.c
@@ -1,22 +1,22 @@
 #include <pthread.h>
 #include <stdio.h>
+#define NUM_THREADS 5
 
 void *PrintHello(void *threadId) {
-	long tId;
-	tId = (long) threadId;
+	long tId = (long) threadId;
 	printf("Hello World! It's me, thread #%ld!\n", tId);
-	pthread_exe(NULL);
+	pthread_exit(NULL);
 }
 
 int main(int argc, char*argv[]) {
 	pthread_t threads[NUM_THREADS];
-	int rc;
 	long t;
-	for (t=0; t<NUM_THREADS>; t++) {
+	int rc;
+	for (t = 0; t < NUM_THREADS; t++) {
 		printf("In main: creating thread %ld\n", t);
 		rc = pthread_create(&threads[t], NULL, PrintHello, (void *)t);
 		if (rc) {
-			printf("ERROR; return code from pthread_create() is %d\n", rc)
+			printf("ERROR; return code from pthread_create() is %d\n", rc);
 			exit(-1);
 		}
 	}
