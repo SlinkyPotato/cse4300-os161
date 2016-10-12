b9d0d536af860e88d40a9e2778dc507c5c57eb93
Amaredeus <patinobrian@gmail.com>
2016-10-11 23:38:55 -0400

    Add Pointers Examples
    * fix b1.c errors

--- Assignment_2/b1.c
+++ Assignment_2/b1.c
@@ -2,6 +2,9 @@
 #include <pthread.h>
 #include <stdlib.h>
 
+// Defined Functions
+void * printThread(void *);
+
 // Global Variables
 #define NUM_THREADS 5
 int last_threadID = 1;
@@ -15,7 +18,37 @@ struct threadArgs {
 
 struct threadArgs threadArgsArray[NUM_THREADS];
 
-void *printThread(void *pThreadArgs) {
+int main(int argc, char const *argv[]) {
+  int taskIds[NUM_THREADS];
+  // struct threadArgs *threadData;
+  void *status;
+  pthread_attr_t attr;
+
+  pthread_mutex_init(&mutex, NULL);
+
+  // Create theads to perform calculation
+  pthread_attr_init(&attr);
+  pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+
+  for (int i = 0; i < NUM_THREADS; i++) {
+    taskIds[i] = i + 1;
+    threadArgsArray[i].threadId = taskIds[i];
+    int rc = pthread_create(&threads[i], NULL, printThread, &threadArgsArray[i]);
+    if (rc) {
+      printf("An error has occured while trying to create a pthread.");
+      exit(-1);
+    }
+  }
+  pthread_attr_destroy(&attr);
+
+  for(int i=0; i < NUM_THREADS; i++) {
+    pthread_join(threads[i], &status);
+  }
+  pthread_mutex_destroy(&mutex);
+  pthread_exit(NULL);
+}
+
+void * printThread(void * pThreadArgs) {
   struct threadArgs *pthreadArgs = (struct threadArgs *) pThreadArgs;
   int threadId = pthreadArgs->threadId;
   int numOfCalls = threadArgsArray[threadId].numOfCalls;
@@ -23,7 +56,7 @@ void *printThread(void *pThreadArgs) {
   // while (numOfCalls < 10) {
     // printf("Inside numofCalls: %d\n", threadId);
     pthread_mutex_lock (&mutex);
-    printf("Is %d locked? %d\n", threadId);
+    printf("Is %d locked?", threadId);
     // if (didLock) {
     //   int differenceThread = threadId - last_threadID;
     //   if (differenceThread == 0) {
@@ -37,34 +70,4 @@ void *printThread(void *pThreadArgs) {
     // }
     pthread_exit((void*) 0);
   // }
-}
-
-int main(int argc, char const *argv[]) {
-  int taskIds[NUM_THREADS], rc;
-  struct threadArgs *threadData;
-  void *status;
-  pthread_attr_t attr;
-
-  pthread_mutex_init(&mutex, NULL);
-
-  // Create theads to perform calculation
-  pthread_attr_init(&attr);
-  pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
-
-  for (int i = 1; i <= NUM_THREADS; i++) {
-    taskIds[i] = i;
-    threadArgsArray[i].threadId = i;
-    rc = pthread_create(&threads[i], NULL, printThread, &threadArgsArray[i]);
-    if (rc) {
-      printf("An error has occured while trying to create a pthread.");
-      exit(-1);
-    }
-  }
-  pthread_attr_destroy(&attr);
-
-  for(int i=0; i < NUM_THREADS; i++) {
-    pthread_join(threads[i], &status);
-  }
-  pthread_mutex_destroy(&mutex);
-  pthread_exit(NULL);
 }
